d2b7f3d60574f8c6b5d660dcabe404c48c42f438
IsmoiljonJabborov <190107106@stu.sdu.edu.kz>
2022-10-01 16:06:11 +0500

    I did the tasks these are very clear!

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,10 @@
 package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
-    public void printGreatest(int first, int second) {
+    public static void printGreatest(int first, int second) {
+        if (first >= second) {
+            System.out.println(first);
+        } else
+            System.out.println(second);
     }
 }
